3ebf481186c79af51e15416b9c77cf0f8e3e587b
ahillman2000 <alexander.hillman0000@gmail.com>
2021-05-15 17:02:16 +0100

    added level, enemy prefabs and weapons

---
+++ Rooty Tooty Point and Shooty/Assets/Scripts/FireSandwichScript.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FireSandwichScript : MonoBehaviour
+{
+    public GameObject projectile;
+    public Transform barrelEnd;
+    Camera mainCamera;
+
+    void Start()
+    {
+        mainCamera = Camera.main;
+    }
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            int x = Screen.width / 2;
+            int y = Screen.height / 2;
+
+            Ray ray = mainCamera.ScreenPointToRay(new Vector3(x, y));
+
+            if (Physics.Raycast(ray, out RaycastHit hit))
+            {
+                Quaternion hitObjectRotation = Quaternion.LookRotation(hit.normal);
+                //Instantiate(projectile, hit.point, hitObjectRotation);
+                Instantiate(projectile, hit.point, hitObjectRotation);
+            }
+        }
+    }
+}
