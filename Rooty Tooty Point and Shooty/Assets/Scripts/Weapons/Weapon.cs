c13451631a8f796893269cc4efbf55cabff0a859
ahillman2000 <alexander.hillman0000@gmail.com>
2021-05-18 14:21:00 +0100

    ammo, reloading and animation added

--- Rooty Tooty Point and Shooty/Assets/Scripts/Weapons/Weapon.cs
+++ Rooty Tooty Point and Shooty/Assets/Scripts/Weapons/Weapon.cs
@@ -1,4 +1,5 @@
 ﻿using UnityEngine;
+using System.Collections;
 
 public class Weapon : MonoBehaviour
 {
@@ -6,20 +7,43 @@ public class Weapon : MonoBehaviour
     public float damage = 10f;
     public float range = 100f;
     public float fireRate = 15f;
+    float nextTimeToFire = 0f;
 
     Camera mainCamera;
     public ParticleSystem muzzleFlash;
     public GameObject impactEffectTerrain;
 
-    float nextTimeToFire = 0f;
+    public Animator animator;
+    public int maxAmmo;
+    private int currentAmmoCapacity;
+    public float reloadTime;
+    private bool isReloading = false;
 
     private void Start()
     {
+        currentAmmoCapacity = maxAmmo;
         mainCamera = Camera.main;
+
+    }
+
+    private void OnEnable()
+    {
+        isReloading = false;
+        animator.SetBool("Reloading", false);
     }
 
     void Update()
     {
+        if(isReloading)
+        {
+            return;
+        }
+
+        if(currentAmmoCapacity == 0 || ((currentAmmoCapacity < maxAmmo) && Input.GetKey(KeyCode.R)))
+        {
+            StartCoroutine(Reload());
+            return;
+        }
         if(automatic)
         {
             if (Input.GetMouseButton(0) && Time.time >= nextTimeToFire)
@@ -42,6 +66,8 @@ public class Weapon : MonoBehaviour
     {
         muzzleFlash.Play();
 
+        currentAmmoCapacity--;
+
         RaycastHit hit;
         if(Physics.Raycast(mainCamera.transform.position, mainCamera.transform.forward, out hit, range))
         {
@@ -57,5 +83,19 @@ public class Weapon : MonoBehaviour
             GameObject _impactDecalTerrain = Instantiate(impactEffectTerrain, hit.point, Quaternion.LookRotation(hit.normal));
             Destroy(_impactDecalTerrain, 10f);
         }
+    }
+
+    IEnumerator Reload()
+    {
+        isReloading = true;
+        Debug.Log("Reloading");
+        animator.SetBool("Reloading", true);
+
+        yield return new WaitForSeconds(reloadTime - 0.25f);
+        animator.SetBool("Reloading", false);
+        yield return new WaitForSeconds(0.25f);
+
+        currentAmmoCapacity = maxAmmo;
+        isReloading = false;
     }
 }
